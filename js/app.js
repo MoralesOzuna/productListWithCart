be056a9e3d7fc62f71bfb2fa0b0fbafd5da17f90
Jose Morales <moraleseduardoo541@gmail.com>
2025-06-15 18:31:37 -0700

    Protoype of Cart ALMOST FINISHED

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@
 
 //1. Object constructor mas reutilizable, nos permite crear múltiples objetos con la misma estructura y lógica sin tener que repetir código.
 
+
+/* CONSTRUCTORES como UI no va a tener metodos que retornen propiedades se deja vacio */
 function Cart(nombre, cantidad, precio, total){
     this.nombre = nombre;
     this.cantidad = cantidad;
@@ -17,32 +19,12 @@ function Cart(nombre, cantidad, precio, total){
     this.total = total;
 }
 
-Cart.prototype.cartElements = function(){
-
-    //Si cantidad = 1; devuelve la info
-    if(cantidad === 1 ){
-        return {
-            nombre: nombre,
-            cantidad: cantidad,
-            precio: precio,
-            total: total
-        }
-    }
-    //Si cantidad > 2 devuelve solo la cantidad actualizada
-    if(cantidad > 1) {
-        return {
-            cantidad: cantidad
-        }
-    }
-    return;   
-}
-
 function UI(){
 
 }
 
-
-UI.prototype.mostrarProductos = function(productos){
+//Mostramos la lista de productos
+UI.prototype.mostrarProductos = function(productos){ //Productos proviene de database.js
     productos.forEach( producto => {
 
         const article = document.createElement('ARTICLE');
@@ -58,128 +40,189 @@ UI.prototype.mostrarProductos = function(productos){
             </button>
         </div>
 
-            <div class = "information">
-                <p class = "information__tag"> ${producto.category} </p>
-                <h2 class = "information__name"> ${producto.name} </h2>
-                <p class = "information__price"> $${producto.price} </p>
-            </div>        
+        <div class = "information">
+            <p class = "information__tag"> ${producto.category} </p>
+            <h2 class = "information__name"> ${producto.name} </h2>
+            <p class = "information__price"> $${producto.price} </p>
+        </div>        
         `
 
     document.querySelector('.products').appendChild(article);
-
-
-
-  
         
     });
 
-    ui.productButton();
+    this.productButton();
 }
+
 
 UI.prototype.productButton = function(){
    
-
     articles = document.querySelectorAll('.product');
 
     articles.forEach( article =>{
 
+        //Container donde se encuentra el boton
         const buttonContainer = article.querySelector('.img-container'); 
-
         //AddButton es el boton para agregar al carrito.
         const addButton = article.querySelector('.product__button');
-        //AddButtonActive es el boton que nos permite seleccionar cantidad.
+        //AddButtonActive es el "nuevo" boton que nos permite seleccionar cantidad.
         const addButtonActive = document.createElement('BUTTON');
-
 
         addButton.addEventListener('click', () =>{
             //Desaparece el boton de agregar al carrito
             addButton.style.display = 'none';
     
-            //Agregamos el nuevo boton y estilos
+            //Agregamos el nuevo boton al container
             addButtonActive.classList.add('product__button--active');
             buttonContainer.appendChild(addButtonActive);
-    
-            //Si existe la clase en el boton activo, ya no duplicamos el texti
-       
-            if(addButtonActive.classList.contains('buttonActive')) return;
+
             addButtonActive.style.display = 'block';
             addButtonActive.textContent = ''; //Reseteamos texto del boton
             addButtonActive.classList.add('buttonActive'); //Nos da unos estilos adicionales de boton
     
-            //Agregamos el elemento Span que tiene el numero
+            //Agregamos el elemento Span que tiene el numero del boton activo
             const addSpan = document.createElement('SPAN'); 
             addSpan.textContent = '1';
-    
             addButtonActive.appendChild(addSpan);
-            //Agregamos las imagenes para agregar y disminuir
-            const quantitySubtract = document.createElement('img');
-            quantitySubtract.src = "assets/images/icon-decrement-quantity.svg";
-            quantitySubtract.classList.add('product__add', 'more');
-    
-            const quantityAdd = document.createElement('img');
-            quantityAdd.src =  "assets/images/icon-increment-quantity.svg";
-            quantityAdd.classList.add('product__add', 'less');
-    
-            addButtonActive.appendChild(quantityAdd);
-            addButtonActive.appendChild(quantitySubtract);
 
-            //Creamos la instancia de cart
-
-            nombre = article.querySelector('.information__tag').textContent;
-            cantidad = Number.parseInt(addSpan.textContent);
-            precio = parseFloat(
-                article.querySelector('.information__price').textContent
-                .replace(/[^0-9.]/g, ''));
-            total = [];
-
-
-            const cart = new Cart(nombre, cantidad, precio, total);
-
-            const elements = cart.cartElements();
-            
-            
-            console.log(elements);
-            
-            
-       
-    
-            //Eventos que detonan los iconos de + y -
-            quantityAdd.addEventListener('click', () =>{
-                ui.sumarCantidad(addSpan);
-              
-                nombre = article.querySelector('.information__tag').textContent;
-                cantidad = Number.parseInt(addSpan.textContent);
-                precio = parseFloat(
-                    article.querySelector('.information__price').textContent
-                    .replace(/[^0-9.]/g, ''));
-                total = [...total, precio];
-    
-                //Creamos una nueva instancia del carrito;
-                const cart = new Cart(nombre, cantidad, precio, total);
-
-                const elements = cart.cartElements();
-                
-                
-                console.log(elements);
-
-            })
-    
-            quantitySubtract.addEventListener('click', () =>{
-                ui.restarCantidad(addSpan, addButtonActive, addButton);
-                const elements = cart.cartElements();
-            })
+            this.quantityButtons(addButtonActive, addSpan, addButton, article);
         })
     })
 }
 
 
-UI.prototype.sumarCantidad = function(textBoton){
+UI.prototype.quantityButtons = function(BotonPadre, addSpan, addButton, article){
+    //Agregamos las imagenes para agregar y disminuir
+    const quantitySubtract = document.createElement('img');
+    quantitySubtract.src = "assets/images/icon-decrement-quantity.svg";
+    quantitySubtract.classList.add('product__add', 'more');
+
+    const quantityAdd = document.createElement('img');
+    quantityAdd.src =  "assets/images/icon-increment-quantity.svg";
+    quantityAdd.classList.add('product__add', 'less');
+
+    BotonPadre.appendChild(quantityAdd);
+    BotonPadre.appendChild(quantitySubtract);
+
+    TomarDatos(article, addSpan); 
+
+    //Al agregar o disminuir cambia el numero
+    quantityAdd.addEventListener('click', () =>{
+        this.sumarCantidad(addSpan, article);
+    })
+
+
+    quantitySubtract.addEventListener('click', () =>{
+        this.restarCantidad(addSpan, BotonPadre, addButton);
+    })
+
+    const cart = new Cart(nombre, cantidad, precio, total);
+    const cartList = cart.cartElements();
+
+    ui.mostrarResultado(cart, cartList);
+
+}
+
+
+function TomarDatos(article, addSpan){
+
+    //Creamos un objeto con la informacion del producto seleccionado
+    const infoProducto = {
+        nombre: article.querySelector('.information__tag').textContent,
+        cantidad: Number.parseInt(addSpan.textContent),
+        precio: parseFloat(article.querySelector('.information__price').textContent.replace(/[^0-9.]/g, '')),
+        total: Number.parseInt(addSpan.textContent) * parseFloat(article.querySelector('.information__price').textContent.replace(/[^0-9.]/g, ''))
+
+    }
+
+    const cart = new Cart(
+        infoProducto.nombre,
+        infoProducto.cantidad,
+        infoProducto.precio,
+        infoProducto.total
+    );
+
+     const elements = cart.cartElements();
+}
+
+UI.prototype.mostrarResultado = function(cart, cartList){
+
+    let {nombre, cantidad, precio, total} = cart;
+    cartAside = document.querySelector('.cart');
+    cartTitle = document.querySelector('.cart__title');
+    /* cartNumber = cartTitle.querySelector('span').textContent; */
+    cartImage = document.querySelector('.cart__image');
+    cartMessage = document.querySelector('.cart__message');
+
+    cartTitle.style.textAlign = 'left';
+    cartImage.style.display = 'none';
+    cartMessage.style.display = 'none';
+
+    listContainer = document.createElement('DIV');
+    cartAside.appendChild(listContainer);
+
+    console.log(cartList)
+  
+    
+
+
+    
+    listContainer.innerHTML =  `
+
+    <ul class = "productos">
+        <li class = "producto">
+            <p class = "producto__nombre"> ${nombre} </p>
+            <p class = "producto__cantidad"> ${cantidad} </p>
+            <p class = "producto__precio"> ${precio} </p>
+        
+
+        </li>
+
+
+    
+    
+    
+    `
+    
+
+
+
+
+    //Crea el carrito
+
+  
+
+}
+
+
+
+
+
+Cart.prototype.cartElements = function(){
+     cantidad = this.cantidad;
+     nombre = this.nombre;
+     precio = this.precio;
+     total = this.total;
+
+     console.log(this.cantidad)
+
+
+    return {
+        nombre: nombre,
+        cantidad: cantidad,
+        precio: precio,
+        total: total
+        
+
+    }
+
+}
+
+
+UI.prototype.sumarCantidad = function(textBoton, article){
     let cantidad = Number.parseInt(textBoton.textContent);
     cantidad++;
     textBoton.textContent = cantidad;
-  
-
-
 }
 
 UI.prototype.restarCantidad = function(textBoton, buttonActive, addButton) {
@@ -199,11 +242,57 @@ UI.prototype.restarCantidad = function(textBoton, buttonActive, addButton) {
   
 }
 
+UI.prototype.mostrarResultado = function(cart, cartList){
 
+    let {nombre, cantidad, precio, total} = cart;
+    cartAside = document.querySelector('.cart');
+    cartTitle = document.querySelector('.cart__title');
+    /* cartNumber = cartTitle.querySelector('span').textContent; */
+    cartImage = document.querySelector('.cart__image');
+    cartMessage = document.querySelector('.cart__message');
+
+    cartTitle.style.textAlign = 'left';
+    cartImage.style.display = 'none';
+    cartMessage.style.display = 'none';
+
+    listContainer = document.createElement('DIV');
+    cartAside.appendChild(listContainer);
+
+    console.log(cartList)
+  
+    
+
+
+    
+    listContainer.innerHTML =  `
+
+    <ul class = "productos">
+        <li class = "producto">
+            <p class = "producto__nombre"> ${nombre} </p>
+            <p class = "producto__cantidad"> ${cantidad} </p>
+            <p class = "producto__precio"> ${precio} </p>
+        
+
+        </li>
+
+
+    
+    
+    
+    `
+    
+
+
+
+
+    //Crea el carrito
+
+  
+
+}
 
 //2. Instanciar UI
 const ui = new UI(); //Es decir creamos un nuevo objeto de la clase particular UI
-
 
 
 
@@ -213,3 +302,5 @@ document.addEventListener('DOMContentLoaded', () =>{
     
     
 })
+
+
---
+++ js/app2.js
@@ -0,0 +1,288 @@
+/* Constructores 
+
+Es una función especial que se utiliza para crear nuevos objetos con una misma estructura.
+*/
+
+
+let cartTotal = [];
+
+function Cart(name, category, number, price, subtotal, total){
+    this.name = name;
+    this.category = category;
+    this.number = number;
+    this.price = price;
+    this.subtotal = this.number * this.price;
+    this.total =+ this.subtotal;
+}
+function UI(){
+
+}
+/* Load the products from database.js to UI */
+UI.prototype.loadProducts = function (){
+    const container = document.querySelector('.products');
+    container.innerHTML = '';
+
+
+    products.forEach(product => {
+        //Destructuring de objetos
+        const {category, name, price, image} = product;
+        const article = document.createElement('article');     
+        article.classList.add('product'); 
+
+        article.innerHTML = `
+        <div class = "img-container">
+
+            <picture>
+                <source media="(min-width: 1024px)" srcset="${image.desktop}">
+                <source media="(min-width: 768px)" srcset="${image.tablet}">
+                <img src="${image.mobile}" alt="product">
+            </picture>
+        
+            <button class =  "product__button">
+                <img src = "../assets/images/icon-add-to-cart.svg" alt = "Cart icon">Add to Cart     
+            </button>
+        </div>
+
+        <div class = "information">
+            <p class = "information__tag"> ${category} </p>
+            <h2 class = "information__name"> ${name} </h2>
+            <p class = "information__price"> $${price.toFixed(2)}</p>        
+        </div>
+        `
+
+        container.appendChild(article);
+
+    });   
+
+    this.addButton();
+}
+
+UI.prototype.addButton = function(){
+
+    /* Select all products */    
+    const articles = document.querySelectorAll('.product');
+
+    articles.forEach(article =>{
+        const addtoCartButton = article.querySelector('.product__button');
+        
+        addtoCartButton.addEventListener('click', () =>{
+            //Dissapear add to cart button to create the new one
+            addtoCartButton.style.display = 'none';
+            
+            const imgContainer = article.querySelector('.img-container');
+            const quantityButtonExist = imgContainer.querySelector('.product__button--active');
+
+            //If button exist we don't create a new one instead just display the inline style atrribuite
+            if(quantityButtonExist){  
+                quantityButtonExist.style.display = 'inline';
+                getProduct(article);
+                cartTotal.forEach(producto=>{
+                    producto.number = 1;
+                   
+                 })
+           
+            }
+
+            //Create a new button if doesn't exist
+            else{
+                const quantityButton = document.createElement('BUTTON');      
+                quantityButton.classList.add('product__button--active', 'buttonActive');
+    
+                //Add the new button to HTML
+                const imgContainer = article.querySelector('.img-container');
+                imgContainer.appendChild(quantityButton);
+    
+                //add buttonActive text
+                const quantityAmount = document.createElement('SPAN');
+                quantityAmount.classList.add('product__amount');
+                quantityAmount.textContent = '1'
+                quantityButton.appendChild(quantityAmount);
+    
+                //Add increment and decrement buttons
+                const quantityAdd = document.createElement('img');
+                quantityAdd.src = '../assets/images/icon-increment-quantity.svg';
+                quantityAdd.alt = 'increment icon';
+                quantityAdd.classList.add('product__add', 'right');
+    
+                const quantityLess = document.createElement('img');
+                quantityLess.src = '../assets/images/icon-decrement-quantity.svg';
+                quantityLess.alt = 'decrement icon';
+                quantityLess.classList.add('product__add', 'left');
+    
+                quantityButton.appendChild(quantityLess);
+                quantityButton.appendChild(quantityAdd);
+
+                getProduct(article);
+                
+
+            quantityAdd.addEventListener('click', () =>{
+                const name = article.querySelector('.information__name').textContent;
+             
+                const cartItem = cartTotal.find(producto => producto.name == name);
+                
+                if(cartItem){
+
+                    cartItem.increment();
+                    ui.cartHTML();
+                    quantityAmount.textContent = cartItem.number;
+                }
+            })
+
+            quantityLess.addEventListener('click', () =>{
+                const name = article.querySelector('.information__name').textContent;
+                const cartItem = cartTotal.find(producto => producto.name === name);
+
+                if(cartItem){
+                    cartItem.decrement();
+                    ui.cartHTML();
+                    quantityAmount.textContent = cartItem.number;
+
+                    
+                if(cartItem.number < 1){
+                    quantityButton.style.display = 'none';
+                    addtoCartButton.style.display = 'block';
+                    quantityAmount.textContent = 1;
+                    cartTotal = cartTotal.filter(producto => producto !== cartItem);
+                    ui.cartHTML();
+
+                }
+                }
+
+            })
+
+            }
+        })
+        
+    })
+}
+
+Cart.prototype.increment = function(){
+    this.number++;
+    this.subtotal = this.number * this.price;
+   
+}
+
+Cart.prototype.decrement = function(){
+    this.number--;
+    this.subtotal = this.number * this.price;
+ 
+}
+
+getProduct = function(article) { 
+    /* la funcion getProduct está tomando datos del HTML y luego creando un nuevo objeto Cart con esos datos. Eso no tiene sentido que sea un método de Cart.prototype, porque aún no tienes ningún Cart. De hecho, lo usas para crear uno. */
+
+    const infoProduct = {
+        name: article.querySelector('.information__name').textContent,
+        category: article.querySelector('.information__tag').textContent,
+        price: parseFloat(article.querySelector('.information__price').textContent.replace(/[^0-9.]/g, '')),
+        number: 1
+    }
+
+    const cartItem =  new Cart(infoProduct.name, infoProduct.category, infoProduct.number, infoProduct.price);
+  
+   
+  
+    cartTotal.push(cartItem);    
+
+    ui.cartHTML();
+    
+}
+
+UI.prototype.cartHTML = function(){
+    const cartContainer = document.querySelector('.cart');
+    const cartImage = document.querySelector('.cart__image');
+    const cartMessage = document.querySelector('.cart__message');
+    const cartList = document.querySelector('.cart__list');
+    const cartItem = document.querySelector('.cart__item');
+    let total = 0;
+    let totalItems = 0;
+
+
+    cartImage.style.display = 'none'
+    cartMessage.style.display = 'none';
+ 
+
+
+    if(cartItem.querySelectorAll('.purchase')){
+        cartItem.querySelectorAll('.purchase').forEach(el => el.remove());
+    }
+    
+
+   cartTotal.forEach(product =>{
+
+    totalItems += product.number;
+
+    const cartNumber = document.querySelector('.cart__number');
+    cartNumber.textContent = totalItems;
+
+
+    const purchase = document.createElement('li');
+    purchase.classList.add('purchase');
+
+    const purchaseName = document.createElement('P');
+    purchaseName.textContent = product.name;
+    purchaseName.classList.add('purchase__name');
+
+    const purchaseNumber = document.createElement('P');
+    purchaseNumber.textContent = `${product.number}x`;
+    purchaseNumber.classList.add('purchase__number');
+
+    const purchasePrice = document.createElement('P');
+    purchasePrice.textContent = `@ $${product.price}`;
+    purchasePrice.classList.add('purchase__price');
+
+    const purchaseSubtotal = document.createElement('P');
+    purchaseSubtotal.textContent = `$${product.subtotal}`
+    purchaseSubtotal.classList.add('purchase__subtotal');
+
+    total += product.subtotal;
+    const purchaseTotal = document.createElement('P');
+    purchaseTotal.textContent = `$${total}` 
+    purchaseTotal.classList.add('purchase__total')
+
+
+
+
+
+    purchase.appendChild(purchaseName);
+    purchase.appendChild(purchaseNumber);
+    purchase.appendChild(purchasePrice);
+    purchase.appendChild(purchaseSubtotal);
+    purchase.appendChild(purchaseTotal);
+
+    
+   /*  purchase.classList.add('purchase__name');
+    purchase.textContent = product.name; */
+    
+    cartItem.appendChild(purchase)
+
+
+
+
+
+
+  })
+  
+
+
+
+
+
+     
+    
+    
+   
+
+
+    
+}
+
+addEventListener('DOMContentLoaded', () =>{
+    ui.loadProducts();
+})
+
+
+/* Instanciamos
+Creamos una instancia de la funcion constructora. 
+*/
+const ui = new UI();
